71eba8b077268699a891d25b712cbd58b8d7e4f8
saidshah <saidmshah93@gmail.com>
2022-11-29 12:23:40 -0600

    functions lesson added

--- lessons/functions/functions-lesson.js
+++ lessons/functions/functions-lesson.js
@@ -1,7 +1,7 @@
 //
 (function (){  //We put all our javascript code inside this to prevent early JS code run before HTML and CSS
     
-    console.log("hello functions"); //just checking if the files are connected
+    //console.log("hello functions"); //just checking if the files are connected
 
     //First function
     
@@ -26,9 +26,9 @@
         let answer = (number % 2 == 0);
         return answer;
     }
-    console.log(isEven(1234567456));
-    console.log(isEven(4567876543));
-    console.log(isEven(true));
+    // console.log(isEven(1234567456));
+    // console.log(isEven(4567876543));
+    // console.log(isEven(true));
 })();
 
 // Arrow functions
@@ -38,4 +38,42 @@ const sayHello = (name) =>{
     let message = `Hello ${name}.`
     return message;
 }
-console.log(sayHello("Alex"))
+//console.log(sayHello("Alex"))
+
+
+//################### Practice to creating functions ##############
+
+//1. understand the problem, read and reread it
+//2. Devise the problem and Pseudo code it
+//3. Carry out the plan, and write code
+//4. once its working see if you can make it better
+
+//Example: create a function that take in a string and remove the first letter and returns the rest
+//if no input given, returns false.
+//if one character is given return and empty string
+
+
+//########## Pseudo Code
+//create a function with one argument
+//check is its a string, if not return false
+//check for number of characters
+//if empty return empty
+//remove the first char of the string and return the rest
+
+function removeFirstChar(input){
+    // if(typeof(input) !== 'string')
+    // return false;
+    // if(input.length === 0)
+    //     return input;
+    // else
+    //     return input.slice(1);
+//## In turnary condition way
+    if(typeof input !== 'string') return false;
+    return (input.length === 0) ? input : input.slice(1);
+}
+console.log(removeFirstChar("Hello"), "ello");
+console.log(removeFirstChar("call"), "all");
+console.log(removeFirstChar("1"), "");
+console.log(removeFirstChar("",""));
+console.log(removeFirstChar(" Hello"), "Hello");
+console.log(removeFirstChar(" Hello "), "ello ");
