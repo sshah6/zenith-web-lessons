e3ebbb9ff0dedb347fb7684e62efa7643a27dc41
saidshah <saidmshah93@gmail.com>
2023-01-03 10:14:11 -0600

    warmup 2 added

--- Warmup/warmup.js
+++ Warmup/warmup.js
@@ -9,9 +9,9 @@ function returnLastTwoChars(input){
         return input.slice(-2);
     }
 }
-console.log(returnLastTwoChars("cat"), "at");
-console.log(returnLastTwoChars("apple"), "le");
-console.log(returnLastTwoChars("carrot"), "ot");
+// console.log(returnLastTwoChars("cat"), "at");
+// console.log(returnLastTwoChars("apple"), "le");
+// console.log(returnLastTwoChars("carrot"), "ot");
 
 // EXTRA CHALLENGE
 // Create a function, returnLastCharsReversed that takes in two arguments:
@@ -36,15 +36,32 @@ function returnLastCharsReversed(input, int){
         if(int === 0){
             return "";
         }
-        sliced = sliced.split("");
-        sliced = sliced.reverse();
-        sliced = sliced.join("");
+         sliced = sliced.split("");
+         sliced = sliced.reverse();
+         sliced = sliced.join("");
         return sliced;   
     }
 }
-console.log(returnLastCharsReversed("codeup", 0), "");
-console.log(returnLastCharsReversed("codeup", 1), "p");
-console.log(returnLastCharsReversed("codeup", 2), "pu");
-console.log(returnLastCharsReversed("codeup", 3), "pue");
-console.log(returnLastCharsReversed("codeup", 4), "pued");
-console.log(returnLastCharsReversed("cat", 3), "tac");
+// console.log(returnLastCharsReversed("codeup", 0), "");
+// console.log(returnLastCharsReversed("codeup", 1), "p");
+// console.log(returnLastCharsReversed("codeup", 2), "pu");
+// console.log(returnLastCharsReversed("codeup", 3), "pue");
+// console.log(returnLastCharsReversed("codeup", 4), "pued");
+// console.log(returnLastCharsReversed("cat", 3), "tac");
+
+// ##### 2 ########=========================
+// Write a function, iBeforeE that takes in a string and returns the string with any ‘ei’ characters replaced with ‘ie’.
+// iBeforeE(‘ei’) // returns ‘ie’
+// iBeforeE(‘height’) // returns ‘hieght’
+// iBeforeE(‘heist’) // returns ‘hiest’
+// iBeforeE(‘their’) // returns ‘thier’
+// iBeforeE(‘theirtheir’) // returns ‘thierthier’
+function iBeforeE(input){
+    if(typeof input === 'string'){
+        input = input.replaceAll("ei", "ie");
+        return input;
+    }
+}
+console.log(iBeforeE("ei"), "ie");
+console.log(iBeforeE("theirtheir"), "thierthier");
+console.log(iBeforeE("height"), "hieght");
