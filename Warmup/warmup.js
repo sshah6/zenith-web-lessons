fb3b51dc4dddc74ea044e44eac77edd6aee03b95
saidshah <saidmshah93@gmail.com>
2023-01-11 23:02:49 -0600

    Warmup added

--- Warmup/warmup.js
+++ Warmup/warmup.js
@@ -177,5 +177,13 @@ function getUserCredentials(user){
     }
     return userPass;
 }
-console.log(getUserCredentials(users));
+// console.log(getUserCredentials(users));
+
+for (let i = 0; i < 10; i++) {
+  console.log("The value of i is " + i);
+   i++;
+   i--;
+}
+
+// desired output : 0, 1 ,2,  3, 4, 5, 6, 7, 8, 9
 
