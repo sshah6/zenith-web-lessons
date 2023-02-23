44cabd69b4c720fb4ca9b57c08d407658d9614d9
saidshah <saidmshah93@gmail.com>
2023-02-23 13:51:12 -0600

    file IO

--- JAVA-Lectures/src/exceptions/exceptionMiniExTest.java
+++ JAVA-Lectures/src/exceptions/exceptionMiniExTest.java
@@ -5,12 +5,17 @@ public class exceptionMiniExTest {
         exceptionMiniEx string = new exceptionMiniEx();
         String subString1 = string.str.substring(6);
         System.out.println(subString1);
+
+        //The importance of try and catch block is that when the error happens, the rest of the code after it still runs
+        //Try and catch is for developers to use not for useres
         try{
             String subString2 = string.str.substring(15);
             System.out.println(subString2);
         }catch(Exception e){
             e.printStackTrace();
         }
+
+        System.out.println("Hello");
 
         //.essage exception
 //        catch(Exception e){
---
+++ JAVA-Lectures/src/fileIOLecture/fileIO.java
@@ -0,0 +1,4 @@
+package fileIOLecture;
+
+public class fileIO {
+}
